5ac19db4177ab4d29f9d2718e74cb28afa4f7fa4
zlei <zlei@xpc-jkt.(none)>
2010-06-29 14:30:56 +0700

    fixed UTF8Filter

---
+++ WEB-INF/src/com/manihot/xpc/util/UTF8Filter.java
@@ -0,0 +1,22 @@
+
+
+package com.manihot.xpc.util;
+
+import java.io.IOException;
+import javax.servlet.*;
+public class UTF8Filter implements Filter
+{
+public void destroy() {}
+public void doFilter(ServletRequest request,
+ServletResponse response,
+FilterChain chain)
+throws IOException, ServletException
+{
+request.setCharacterEncoding("UTF-8"); 
+chain.doFilter(request, response);
+}
+ public void init(FilterConfig filterConfig)
+throws ServletException {}
+}
+
+
