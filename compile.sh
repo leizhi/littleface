71e3595855d3d0c1dfc22e4c56426390779cac7a
leizhi <leizhifesker@gmail.com>
2010-07-29 01:44:15 +0700

    fixed 4.7

--- compile.sh
+++ compile.sh
@@ -1,40 +1,53 @@
 #!/bin/bash
 
-cdir=`pwd`
-JAVA_HOME=/usr/java/j2sdk
-CATALINA_HOME=/home/xpc
-srcdir=$cdir/WEB-INF/src
-classdir=$cdir/WEB-INF/classes/
+PWD=`pwd`
+JAVA_HOME=/usr/java/j2sdk/
+CATALINA_HOME=/home/zlei/apache-tomcat-5.5.30/
+SRC=$PWD/WEB-INF/src/
+CLASSES=$PWD/WEB-INF/classes/
+LIB=$PWD/WEB-INF/lib/
 
-export CLASSPATH=.:$classdir
-libfilelist=`find $cdir/WEB-INF/lib -name "*.jar" -print`
+export CLASSPATH=.:$CLASSES
+
+libfilelist=`find $CATALINA_HOME/common -name "*.jar" -print`
 for lf in $libfilelist
 do
   export CLASSPATH=$CLASSPATH:$lf
 done
-echo $CLASSPATH
-cd $srcdir
 
-jflist=`find  -name "*.java" -print`
+libfilelist=`find $CATALINA_HOME/shared -name "*.jar" -print`
+for lf in $libfilelist
+do
+  export CLASSPATH=$CLASSPATH:$lf
+done
+
+libfilelist=`find $LIB -name "*.jar" -print`
+for lf in $libfilelist
+do
+  export CLASSPATH=$CLASSPATH:$lf
+done
+
+echo $CLASSPATH
+
+cd $SRC
+
+jflist=`find -name "*.java" -print`
 for jf in $jflist
 do
   cf0=`echo $jf | sed s/\.java$/\.class/`
 #  cf=`echo $cf0 | sed s/^\.//`
-  cf=${classdir}${cf0}
-  if [ -f $cf -a $cf -nt $jf ]; then
-     echo "$cf0 is up to date" > /dev/null
+  cf=${CLASSES}${cf0}
+  #if [ -f $cf -a $cf -nt $jf ]; then
+   #  echo "$cf0 is up to date" > /dev/null
      # echo "$cf0 is up to date"
-  else
-     echo "javac $jf to $cdir/WEB-INF/classes..."
-     $JAVA_HOME/bin/javac -d $cdir/WEB-INF/classes/ $jf
+  #else
+   #  echo "javac $jf to $cdir/WEB-INF/classes..."
+    # $JAVA_HOME/bin/javac -d $CLASSES $jf
+  #fi
+  if [ $cf -ot $jf ]; then
+     echo "javac $jf to $PWD/WEB-INF/classes..."
+     $JAVA_HOME/bin/javac -d $CLASSES $jf
   fi
-# Alternative way
-#  bjf=`basename $jf`
-#  njf=`find . -name $bjf -newer $cf -print`
-#  if [ -n $njf -a $njf == $jf ]; then
-#     javac $jf
-#  fi
 done
 
-#cd $cdir
-
+cd $PWD
