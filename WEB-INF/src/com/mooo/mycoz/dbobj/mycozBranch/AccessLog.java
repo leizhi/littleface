ba8413015151041c56ce0cec13f5ae6f5a4e12e3
leizhi <leizhifesker@gmail.com>
2010-08-05 14:41:23 +0800

    fixed 5.8

---
+++ WEB-INF/src/com/mooo/mycoz/dbobj/mycozBranch/AccessLog.java
@@ -0,0 +1,42 @@
+package com.mooo.mycoz.dbobj.mycozBranch;
+
+import java.sql.Timestamp;
+
+import com.mooo.mycoz.dbobj.DBObject;
+
+public class AccessLog extends DBObject{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1500604579062955853L;
+	
+	private Integer id;
+	private String ip;
+	private Timestamp startdate;
+	private Timestamp enddate;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public Timestamp getStartdate() {
+		return startdate;
+	}
+	public void setStartdate(Timestamp startdate) {
+		this.startdate = startdate;
+	}
+	public Timestamp getEnddate() {
+		return enddate;
+	}
+	public void setEnddate(Timestamp enddate) {
+		this.enddate = enddate;
+	}
+}
