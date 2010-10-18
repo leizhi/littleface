3afc6229be3459e232a2416036329ed0c9ed9829
leizhi <leizhifesker@gmail.com>
2010-10-18 17:25:29 +0800

    fixed 11.4

---
+++ jsp/js/zh.js
@@ -0,0 +1,23 @@
+formcheckLanguage = {
+	required: " 输入不能为空.",
+	alpha: "This field accepts alphabetic characters only.",
+	alphanum: "This field accepts alphanumeric characters only.",
+	nodigit: "No digits are accepted.",
+	digit: "Please enter a valid integer.",
+	digitmin: "The number must be at least %0",
+	digitltd: "The value must be between %0 and %1",
+	number: "请输入数字.",
+	email: "请输入正确的邮箱地址: <br /><span>E.g. yourname@domain.com</span>",
+	phone: "请输入电话号码.",
+	url: "Please enter a valid url: <br /><span>E.g. http://www.domain.com</span>",
+	
+	confirm: "This field is different from %0",
+	differs: "This value must be different of %0",
+	length_str: "The length is incorrect, it must be between %0 and %1",
+	length_fix: "The length is incorrect, it must be exactly %0 characters",
+	lengthmax: "The length is incorrect, it must be at max %0",
+	lengthmin: "The length is incorrect, it must be at least %0",
+	checkbox: "Please check the box",
+	radios: "Please select a radio",
+	select: "Please choose a value"
+}
